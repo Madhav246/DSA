8e44f0f302eb1275f6d74d3709d734af0a904203
Madhav Agrawal <54050193+Madhav246@users.noreply.github.com>
2022-11-01 12:18:36 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (85.68%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -13,19 +13,20 @@ class Solution {
             }
             mid = start + (end - start) / 2;
         }
+
         return end;
     }
 
-    public int binarySearch(int[] nums, int start, int end, int key) {
+    public int binarySearch(int[] nums, int start, int end, int target) {
         int mid = start + (end - start) / 2;
 
         while (start <= end) {
-            if (nums[mid] == key) {
+            if (nums[mid] == target) {
                 return mid;
-            } else if (key < nums[mid]) {
-                end = mid - 1;
-            } else {
+            } else if (target > nums[mid]) {
                 start = mid + 1;
+            } else {
+                end = mid - 1;
             }
             mid = start + (end - start) / 2;
         }
@@ -35,6 +36,7 @@ class Solution {
     public int search(int[] nums, int target) {
         int n = nums.length;
         int pivot = findPivot(nums, n);
+
         if (target >= nums[pivot] && target <= nums[n - 1]) {
             return binarySearch(nums, pivot, n - 1, target);
         } else {
