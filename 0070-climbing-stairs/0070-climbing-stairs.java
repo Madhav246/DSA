52f6447107794c803a8dc430ebc4bdf98a602d79
Madhav Agrawal <54050193+Madhav246@users.noreply.github.com>
2022-11-08 13:52:11 +0530

    Time: 0 ms (100.00%), Space: 40 MB (81.17%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,19 +1,19 @@
 class Solution {
-//Tabulation :
-    //upar se neeche jaa rahe hai !
-    public int solve(int[] dp, int index) {
-       dp[0] = 1;
-       dp[1] = 1;
-        
-       for(int i=2; i<=index; i++){
-           dp[i] = dp[i-1] + dp[i-2];
-       }
-        return dp[index];
+
+    //Space optimization :
+    public int solve(int index) {
+        int prev1 = 1;
+        int prev2 = 1;
+
+        for (int i = 2; i <= index; i++) {
+            int current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return prev1;
     }
 
     public int climbStairs(int n) {
-        int[] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-        return solve(dp, n);
+        return solve(n);
     }
 }
