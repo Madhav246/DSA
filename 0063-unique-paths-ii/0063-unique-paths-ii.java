b2dad0b6932618c8e88e6f7aa1e1d2e1ad24fe74
Madhav Agrawal <54050193+Madhav246@users.noreply.github.com>
2022-11-10 13:06:05 +0530

    Time: 2 ms (37.62%), Space: 42.4 MB (29.08%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,37 +1,41 @@
 class Solution {
 
-    //Memoization :
-    public int solve(int i, int j, int[][] dp, int[][] obstacleGrid) {
-        if (i >= 0 && j >= 0 && obstacleGrid[i][j] == 1) {
-            return 0;
-        }
-
-        if (i == 0 && j == 0) {
-            return 1;
-        }
-
-        if (i < 0 || j < 0) {
-            return 0;
-        }
-
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        int up = solve(i - 1, j, dp, obstacleGrid);
-        int left = solve(i, j - 1, dp, obstacleGrid);
-
-        return dp[i][j] = up + left;
-    }
-
+    //Tabulation
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
+
         int[][] dp = new int[m][n];
 
         for (int[] row : dp) {
             Arrays.fill(row, -1);
         }
-        return solve(m - 1, n - 1, dp, obstacleGrid);
+
+        dp[0][0] = 1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else if (i == 0 && j == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    int up = 0;
+                    int left = 0;
+
+                    if (i > 0) {
+                        up = dp[i - 1][j];
+                    }
+
+                    if (j > 0) {
+                        left = dp[i][j - 1];
+                    }
+
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+
+        return dp[m - 1][n - 1];
     }
 }
