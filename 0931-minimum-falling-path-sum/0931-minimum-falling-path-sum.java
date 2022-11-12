319c0e8c4b1c7969f952d47fbb086640b1980d5f
Madhav Agrawal <54050193+Madhav246@users.noreply.github.com>
2022-11-12 12:04:05 +0530

    Time: 29 ms (19.57%), Space: 47.5 MB (55.83%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,43 +1,43 @@
 class Solution {
-    //Tabulation :
+//Space optimization :
     public int minFallingPathSum(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
 
-        int[][] dp = new int[m][n];
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
-        }
+        int[] current = new int[m];
+        int[] previous = new int[n];
 
         for (int j = 0; j < n; j++) {
-            dp[0][j] = matrix[0][j];
+            previous[j] = matrix[0][j];
         }
 
         for (int i = 1; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                int up = matrix[i][j] + dp[i - 1][j];
+                int up = matrix[i][j] + previous[j];
                 int leftDiagonal = matrix[i][j];
                 if (j - 1 >= 0) {
-                    leftDiagonal += dp[i - 1][j - 1];
+                    leftDiagonal += previous[j - 1];
                 } else {
                     leftDiagonal += (int) 1e9;
                 }
 
                 int rightDiagonal = matrix[i][j];
                 if (j + 1 < m) {
-                    rightDiagonal += dp[i - 1][j + 1];
+                    rightDiagonal += previous[j + 1];
                 } else {
                     rightDiagonal += (int) 1e9;
                 }
 
-                dp[i][j] = Math.min(up, Math.min(leftDiagonal, rightDiagonal));
+                current[j] = Math.min(up, Math.min(leftDiagonal, rightDiagonal));
             }
+
+            previous = current.clone();
         }
 
-        int mini = dp[n - 1][0];
+        int mini = previous[0];
 
         for (int j = 1; j < n; j++) {
-            mini = Math.min(mini, dp[m - 1][j]);
+            mini = Math.min(mini, previous[j]);
         }
 
         return mini;
