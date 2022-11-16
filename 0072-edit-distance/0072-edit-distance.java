ccad09d7afab434e3abadd720b1ddb53a1c39bb4
Madhav Agrawal <54050193+Madhav246@users.noreply.github.com>
2022-11-16 13:19:30 +0530

    Time: 6 ms (93.14%), Space: 42.5 MB (87.63%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,41 +1,32 @@
 class Solution {
-
-    public int solve(int i, int j, String word1, String word2, int[][] dp) {
-        if (i < 0) {
-            return j + 1;
-        }
-
-        if (j < 0) {
-            return i + 1;
-        }
-
-        if (dp[i][j] != -1) {
-            return dp[i][j];
-        }
-
-        if (word1.charAt(i) == word2.charAt(j)) {
-            return dp[i][j] = solve(i - 1, j - 1, word1, word2, dp);
-        }
-
-        int insert = 1 + solve(i, j - 1, word1, word2, dp);
-        int delete = Math.min(insert, 1 + solve(i - 1, j, word1, word2, dp));
-        int replace = Math.min(delete, 1 + solve(i - 1, j - 1, word1, word2, dp));
-
-        int ans = (int) 1e8;
-        ans = Math.min(ans, replace);
-
-        return dp[i][j] = ans;
-    }
-
+//Tabulation :
     public int minDistance(String word1, String word2) {
         int n = word1.length();
         int m = word2.length();
 
-        int[][] dp = new int[n][m];
+        int[][] dp = new int[n + 1][m + 1];
         for (int[] row : dp) {
             Arrays.fill(row, -1);
         }
 
-        return solve(n - 1, m - 1, word1, word2, dp);
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = i;
+        }
+
+        for (int j = 0; j <= m; j++) {
+            dp[0][j] = j;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = 0 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1]));
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 }
