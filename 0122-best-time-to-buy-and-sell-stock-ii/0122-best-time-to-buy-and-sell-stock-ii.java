0e3d2f92964767c59dae18ae8f14757bf33b39aa
Madhav Agrawal <54050193+Madhav246@users.noreply.github.com>
2022-11-20 13:16:00 +0530

    Time: 5 ms (21.86%), Space: 44.9 MB (16.15%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,33 +1,50 @@
 class Solution {
 
-    public int solve(int index, int canBuy, int[] prices, int n, int[][] dp) {
-        if (index == n) {
-            return 0;
-        }
+//     public int solve(int index, int canBuy, int[] prices, int n, int[][] dp) {
+//         if (index == n) {
+//             return 0;
+//         }
 
-        int profit = 0;
+//         int profit = 0;
 
-        if (dp[index][canBuy] != -1) {
-            return dp[index][canBuy];
-        }
+//         if (dp[index][canBuy] != -1) {
+//             return dp[index][canBuy];
+//         }
 
-        if (canBuy == 1) {
-            profit = Math.max(-prices[index] + solve(index + 1, 0, prices, n, dp), 0 + solve(index + 1, 1, prices, n, dp));
-        } else {
-            profit = Math.max(prices[index] + solve(index + 1, 1, prices, n, dp), 0 + solve(index + 1, 0, prices, n, dp));
-        }
+//         if (canBuy == 1) {
+//             profit = Math.max(-prices[index] + solve(index + 1, 0, prices, n, dp), 0 + solve(index + 1, 1, prices, n, dp));
+//         } else {
+//             profit = Math.max(prices[index] + solve(index + 1, 1, prices, n, dp), 0 + solve(index + 1, 0, prices, n, dp));
+//         }
 
-        return dp[index][canBuy] = profit;
-    }
+//         return dp[index][canBuy] = profit;
+//     }
 
     public int maxProfit(int[] prices) {
         int n = prices.length;
 
-        int[][] dp = new int[n][2];
+        int[][] dp = new int[n + 1][2];
         for (int[] row : dp) {
             Arrays.fill(row, -1);
         }
 
-        return solve(0, 1, prices, n, dp);
+        dp[n][0] = dp[n][1] = 0;
+        
+        for(int index=n-1; index>=0; index--){
+            for(int canBuy=0; canBuy<=1; canBuy++){
+                int profit = 0;
+                if (canBuy == 1) {
+                    profit = Math.max(-prices[index] + dp[index + 1][0], 0 + dp[index + 1][1]);
+               } 
+                else {
+                    profit = Math.max(prices[index] + dp[index + 1][1], 0 + dp[index + 1][0]);
+        }
+
+        dp[index][canBuy] = profit;
+            }
+        }
+        
+        return dp[0][1];
+        
     }
 }
