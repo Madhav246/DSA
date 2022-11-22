c99884ab1a06b6c8fea1937796a2754a6da10937
Madhav Agrawal <54050193+Madhav246@users.noreply.github.com>
2022-11-22 12:14:50 +0530

    Time: 15 ms (45.94%), Space: 79.4 MB (56.30%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,19 +1,26 @@
 class Solution {
+//Space Optimization :
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][][] dp = new int[n + 1][2][3];
+       int[][] after = new int[2][3];
+       int[][] curr = new int[2][3];
 
         for (int index = n - 1; index >= 0; index--) {
             for (int canBuy = 0; canBuy <= 1; canBuy++) {
                 for (int transactionCount = 1; transactionCount <= 2; transactionCount++) {
+                    
                     if (canBuy == 1) {
-                        dp[index][canBuy][transactionCount] = Math.max(-prices[index] + dp[index + 1][0][transactionCount], 0 + dp[index + 1][1][transactionCount]);
-                    } else {
-                        dp[index][canBuy][transactionCount] = Math.max(prices[index] + dp[index + 1][1][transactionCount - 1], 0 + dp[index + 1][0][transactionCount]);
+                        curr[canBuy][transactionCount] = Math.max(-prices[index] + after[0][transactionCount], 0 + after[1][transactionCount]);
+                    } 
+                    
+                    else {
+                        curr[canBuy][transactionCount] = Math.max(prices[index] + after[1][transactionCount - 1], 0 + after[0][transactionCount]);
                     }
+                    
                 }
+                after = curr;
             }
         }
-        return dp[0][1][2];
+        return after[1][2];
     }
 }
