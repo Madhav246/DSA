a92489bfa050728fc8d30f55d9fd7bdadb7945f3
Madhav Agrawal <54050193+Madhav246@users.noreply.github.com>
2022-11-23 00:39:04 +0530

    Time: 5 ms (72.13%), Space: 43.9 MB (41.93%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,34 +1,24 @@
 class Solution {
-    public int solve(int index, int transactionNo, int[] prices, int n, int k, int[][] dp){
-        if(index == n || transactionNo == 2 * k){
-            return 0;
-        }
-        
-        if(dp[index][transactionNo] != -1){
-            return dp[index][transactionNo];
-        }
-        
-        int profit = 0;
-        if(transactionNo % 2 == 0){
-            profit += Math.max(-prices[index] + solve(index + 1, transactionNo + 1, prices, n, k, dp),
-                       0 + solve(index + 1, transactionNo, prices, n, k, dp));
-        }
-        
-        else{
-            profit += Math.max(prices[index] + solve(index + 1, transactionNo + 1, prices, n, k, dp),
-                       0 + solve(index + 1, transactionNo, prices, n, k, dp));
-        }
-        
-        return dp[index][transactionNo] = profit;
-    }
+    //Tabulation :
     public int maxProfit(int k, int[] prices) {
         int n = prices.length;
-        
-        int[][] dp = new int[n][2 * k];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
+
+        int[][] dp = new int[n + 1][2 * k + 1];
+
+        for (int index = n - 1; index >= 0; index--) {
+            
+            for (int transactionNo = 2 * k - 1; transactionNo >= 0; transactionNo--) {
+                
+                if (transactionNo % 2 == 0) { //Buy
+                    dp[index][transactionNo] = Math.max(-prices[index] + dp[index + 1][transactionNo + 1], 0 + dp[index + 1][transactionNo]);
+                } 
+                
+                else { //Sell
+                    dp[index][transactionNo] = Math.max(prices[index] + dp[index + 1][transactionNo + 1], 0 + dp[index + 1][transactionNo]);
+                }
+            }
         }
         
-        return solve(0, 0, prices, n, k, dp);
+        return dp[0][0];
     }
 }
