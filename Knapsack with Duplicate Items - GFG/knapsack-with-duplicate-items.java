7856c2c0ea6a5690501f364b1239b70d4684dfa5
Madhav Agrawal <54050193+Madhav246@users.noreply.github.com>
2022-11-29 11:52:38 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.java
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.java
@@ -36,13 +36,10 @@ class Solution{
     
     static int solve(int index, int weight, int[] val, int[] wt, int[][] dp){
         if(index == 0){
-            if(wt[0] <= weight){
-                return (weight/wt[0]) * val[0];
-            }
-            else{
-                return 0;
-            }
+            if(wt[0] <= weight) return (weight/wt[0]) * val[0];
+            else return 0;
         }
+        
         if(dp[index][weight] != -1){
             return dp[index][weight];
         }
@@ -60,9 +57,23 @@ class Solution{
     static int knapSack(int N, int W, int val[], int wt[])
     {
         int[][] dp = new int[N][W + 1];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
+        
+        for(int i=wt[0]; i<=W; i++){
+            dp[0][i] = (i/wt[0]) * val[0];
         }
-        return solve(N - 1, W, val, wt, dp);
+        
+        for(int index = 1; index<N; index ++){
+            for(int weight=0; weight<=W; weight ++){
+                int notTake = 0 + dp[index - 1][weight];
+                int take = 0;
+                if(wt[index] <= weight){
+                    take = val[index] + dp[index][weight - wt[index]];
+                }
+                
+                dp[index][weight] = Math.max(notTake, take);
+            }
+        }
+        
+        return dp[N - 1][W];
     }
 }
