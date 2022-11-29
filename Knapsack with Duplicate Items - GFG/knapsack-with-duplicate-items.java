9bd69ba8e613bc71ed61757a4d165210753660f8
Madhav Agrawal <54050193+Madhav246@users.noreply.github.com>
2022-11-29 11:41:01 +0530

    Added solution - LeetHub

---
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0){
+            String line1[] = in.readLine().trim().split("\\s+");
+            int N = Integer.parseInt(line1[0]);
+            int W = Integer.parseInt(line1[1]);
+            String line2[] = in.readLine().trim().split("\\s+");
+            int val[] = new int[N];
+            for(int i = 0;i < N;i++)
+                val[i] = Integer.parseInt(line2[i]);
+            String line3[] = in.readLine().trim().split("\\s+");
+            int wt[] = new int[N];
+            for(int i = 0;i < N;i++)
+                wt[i] = Integer.parseInt(line3[i]);
+                
+            Solution ob = new Solution();
+            System.out.println(ob.knapSack(N, W, val, wt));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    
+    static int solve(int index, int weight, int[] val, int[] wt, int[][] dp){
+        if(index == 0){
+            if(wt[0] <= weight){
+                return (weight/wt[0]) * val[0];
+            }
+            else{
+                return 0;
+            }
+        }
+        if(dp[index][weight] != -1){
+            return dp[index][weight];
+        }
+        
+        int notTake = 0 + solve(index - 1, weight, val, wt, dp);
+        int take = 0;
+        if(wt[index] <= weight){
+            take = val[index] + solve(index, weight - wt[index], val, wt, dp);
+        }
+        
+        return dp[index][weight] = Math.max(notTake, take);
+    }
+    
+    
+    static int knapSack(int N, int W, int val[], int wt[])
+    {
+        int[][] dp = new int[N][W + 1];
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
+        }
+        return solve(N - 1, W, val, wt, dp);
+    }
+}
