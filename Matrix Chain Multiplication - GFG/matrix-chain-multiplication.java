779e3b065bbe2009955e772cb887f44b3a9b6cb6
Madhav Agrawal <54050193+Madhav246@users.noreply.github.com>
2022-12-05 20:12:13 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.java
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.java
@@ -29,32 +29,27 @@ class GFG
 //User function Template for Java
 
 class Solution{
-    
-    static int solve(int i, int j, int[] arr, int[][] dp){
-        if(i == j){
-            return 0;
-        }
-        
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        
-        int mini = (int)1e9;
-        
-        for(int k = i; k < j; k++){
-            int steps = arr[i - 1] * arr[k] * arr[j] + solve(i , k, arr, dp) + solve(k + 1, j, arr, dp);
-            mini = Math.min(steps, mini);
-        }
-        
-        return dp[i][j] = mini;
-    }
-    
     static int matrixMultiplication(int N, int arr[])
     {
         int[][] dp = new int[N][N];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
+        
+        for(int i = N - 1; i>= 1; i--){
+            for(int j = i + 1; j < N; j++){
+                int mini = (int)1e9;
+        
+                for(int k = i; k < j; k++){
+                    int steps = arr[i - 1] * arr[k] * arr[j] + 
+                                dp[i][k] + 
+                                dp[k + 1][j];
+                                
+                    mini = Math.min(steps, mini);
+                }
+                
+                dp[i][j] = mini;
+                
+            }
         }
-        return solve(1, N - 1, arr, dp);
+        
+        return dp[1][N - 1];
     }
 }
